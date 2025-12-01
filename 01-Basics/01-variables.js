7f5e805a755ce6aa491a3b5dff8d56091f8c8c23
bc220403526san-tech <bc220403526san@vu.edu.pk>
2025-12-01 10:33:32 +0000

    commit files

---
+++ 01-Basics/01-variables.js
@@ -0,0 +1,16 @@
+const accountId  = 24567;
+let accountEmail = "sana@google.com";
+var accountPass = "12134543";
+accountCity = "Islamabad";
+
+let accountState;
+
+// accountId = 653675634;
+
+accountCity = "Lahore";
+accountEmail = "ali@google.com";
+
+
+console.table([accountId, accountEmail, accountPass, accountCity, accountState]);
+
+
---
+++ 01-Basics/02-dataType.js
@@ -0,0 +1,18 @@
+// "use strict"; //Use all the JS code as newer version.
+
+// alert("Hello! This is JAVASCRIPT full course."); // Not working on Node js this is not a browser. This line is working on browser.
+
+console.log("Hello! This is Javascript full course");
+//This is not a good practice, avoid this.
+
+//Data Types in Javascript
+//String
+//bigint
+//Boolean
+//symbols
+//Number
+//Undefined  // type of undefined is undefine
+//Null // null is a standalone vale.The data type of null is object.
+//objects
+
+console.log(typeof true);

874fa30b40fd54d49e93f3932ac0450fd30cceb9
bc220403526san-tech <bc220403526san@vu.edu.pk>
2025-12-02 04:33:11 +0000

    conversion of data types

---
+++ 01-Basics/03-conversionOperations.js
@@ -0,0 +1,30 @@
+let score = "sana";    //Now its type is NaN
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// console.log(typeof NaN);
+// console.log(typeof undefined);
+
+
+let isLoggedIn = 1;
+
+let inBoolean = Boolean(isLoggedIn);
+console.log(inBoolean);
+
+
+let number = 33;
+
+let numberInString = String(number);
+console.log(numberInString);
+console.log(typeof(numberInString));
+
+
+
+
+
+

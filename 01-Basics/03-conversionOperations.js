de7c7a0626ffe9324810e1dc251dac5e266135cc
bc220403526san-tech <bc220403526san@vu.edu.pk>
2025-12-02 05:33:06 +0000

    Operators in JS

--- 01-Basics/03-conversionOperations.js
+++ 01-Basics/03-conversionOperations.js
@@ -1,14 +1,14 @@
 let score = "sana";    //Now its type is NaN
 
-// console.log(typeof score);
-// console.log(typeof(score));
+console.log(typeof score);
+console.log(typeof(score));
 
 let valueInNumber = Number(score);
-// console.log(typeof(valueInNumber));
-// console.log(valueInNumber);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
 
-// console.log(typeof NaN);
-// console.log(typeof undefined);
+console.log(typeof NaN);
+console.log(typeof undefined);
 
 
 let isLoggedIn = 1;
@@ -22,6 +22,26 @@ let number = 33;
 let numberInString = String(number);
 console.log(numberInString);
 console.log(typeof(numberInString));
+
+
+
+// ************************* OPERATIONS *****************************
+
+let str1 = "Hello!";
+let str2 = " Programming";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+// Postfix Operators
+let x = 3;
+const y = x++;
+console.log(`The value of x is:${x}, The value of y is:${y}`);
+
+//Prefix Operator
+let a = 3;
+const b = ++a;
+console.log(`The value of a is: ${a}, The value of b is: ${b}`);
 
 
 
---
+++ 01-Basics/04-comparison.js
@@ -0,0 +1,11 @@
+// *************** COMPARISON OPERATOR ****************
+
+console.log("2" > 1);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined < 0);
+
+// Use of (===) Operator
+console.log("2" > 1);
+
